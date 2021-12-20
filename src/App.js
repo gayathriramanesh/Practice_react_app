66a3d6e6129a5fd4831767c950e6083bece1704f
gayathriramanesh <gayathriramanesh2001@gmail.com>
2021-12-20 20:38:15 +0530

    react-part1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 
 import React, { Component } from 'react';
+import Menu from './components/menucomponent';
 import './App.css';
+
+
 
 class App extends Component{
   render() {
     return (
       <div>
-            <button className="btn btn-primary">hello click this</button>
+           <Menu/>
       </div>
     );
   }
 }
-
-
 
 export default App;
